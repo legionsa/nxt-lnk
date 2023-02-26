7e71cb8caee881528a4f4338a3429b267c7a1696
legionsa <dskalvinlim@gmail.com>
2023-02-26 18:27:11 +0700

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,23 +2,23 @@ export default {
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        title: 'next lnks by realvjy',
-        url: 'https://vjy.me',
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk.',
+        title: 'Kalvin Lim - Product Designer',
+        url: 'https://kalvinlim.com',
+        description: 'I am a UI Designer and later a Product designer based in Jakarta 🇮🇩 with 5+ years of experience in emerging products.',
         locale: 'en_EN',
         keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
         images: [
             {
                 width: 1200,
                 height: 630,
-                url: `https://vjy.me/preview.jpg`,
+                url: `https://www.notion.so/hikalvin/37140efa49cd4051962e3bfce99705f4?v=12a4c801ca574400b1be08b29f07f7a9&p=cea8f62b09bc46ef8ebd0d39e81c718f&pm=c`,
             },
         ],
-        site_name: 'realvjy',
+        site_name: 'kalvinlim',
     },
     twitter: {
-        handle: '@realvjy',
-        site: 'vjy.me',
+        handle: '@hikalvin',
+        site: 'kalvinlim.com',
         cardType: 'summary_large_image',
     },
 };
