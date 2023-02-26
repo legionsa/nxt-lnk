deaa16e81075dbe37d97f3ec9db322605c9a8a80
legionsa <dskalvinlim@gmail.com>
2023-02-26 12:44:14 +0700

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,20 +1,20 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
-        titleImg: true,
+        name: 'Kalvin Lim',
+        username: '@kalvin.lim',
+        url: 'https://loz.my.id',
+        titleImg: false,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
-        descShow: true,
+        description: 'Hi, I am a UI designer and later a Product designer based in Jakarta 🇮🇩 with 5+ years of experience in emerging products. Helps create values in products to empower people. I am a kinesthetic guy, loving to learn anything by doing, and I believe you cant lie about the process, but you can hack the process. ',
+        descShow: false,
         subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
         author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
+        authorURL: 'https://kalvinlim.com',
         footerText: 'made by'
     },
 ];
